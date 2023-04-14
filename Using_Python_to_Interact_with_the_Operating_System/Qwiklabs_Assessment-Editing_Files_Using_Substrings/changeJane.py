6079b02ad82d2c1d82dbf8b38ec028619f6a4a65
Luis Angel Mendoza Velasco <luisangel.mendozavelasco@outlook.com>
2023-04-13 21:15:24 -0600

    Create Qwiklabs_Assessment-Editing_Files_Using_Substrings

---
+++ Using_Python_to_Interact_with_the_Operating_System/Qwiklabs_Assessment-Editing_Files_Using_Substrings/changeJane.py
@@ -0,0 +1,10 @@
+import sys
+import subprocess
+
+file = sys.argv[1]
+with open(file) as f:
+    for line in f.readlines():
+        source = line.strip()
+        destination = source.replace('jane', 'jdoe')
+        subprocess.run(['mv', source, destination])
+    f.close()
